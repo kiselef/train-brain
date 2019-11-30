c0949ead2b1855f1ab594f9db035d70a0f14705b
mkiselev <arz.kiselev@yandex.ru>
2019-11-30 11:28:11 +0300

    settings added

---
+++ src/components/common/SettingsItemCrease.vue
@@ -0,0 +1,64 @@
+<template>
+    <b-form-group
+        class="mb-1"
+        :label="label"
+    >
+        <b-input-group :prepend="prepend">
+            <b-form-input
+                @keypress.prevent="keyPress"
+                :value="value"
+                type="text"
+            ></b-form-input>
+            <b-input-group-append>
+                <b-button class="crease" variant="primary" @click="increase()" :disabled="value >= max"><b>+</b></b-button>
+                <b-button class="crease" variant="dark" @click="decrease()" :disabled="value <= min"><b>-</b></b-button>
+            </b-input-group-append>
+        </b-input-group>
+    </b-form-group>
+</template>
+
+<script>
+  export default {
+    name: "SettingsItemCrease",
+
+    props: {
+      max: Number,
+      min: Number,
+      label: String,
+      value: Number,
+      prepend: {
+        type: String,
+        default: ''
+      },
+      step: {
+        type: Number,
+        default: 1,
+      },
+    },
+
+    methods: {
+      increase: function () {
+        this.$emit('input', this.value + this.step)
+      },
+
+      decrease: function () {
+        this.$emit('input', this.value - this.step)
+      },
+
+      keyPress: function () {
+        return false
+      },
+    },
+  }
+</script>
+
+<style lang="less" scoped>
+    .btn {
+        &.crease {
+            padding: 0.375rem 0.95rem;
+        }
+    }
+    input {
+        text-align: center;
+    }
+</style>
--- src/components/flash-anzan/FlashAnzanSettings.vue
+++ src/components/flash-anzan/FlashAnzanSettings.vue
@@ -2,19 +2,76 @@
     <div>
         <settings-wrapper :is-ready="isReady">
             <div class="row">
-                <div class="col-sm-6 col-lg-3">
-                    <b-form-group
-                        class="mb-1"
-                        label="Число элементов"
-                        label-for="multiply-form_numberOfElements"
-                        label-cols="8"
-                    >
-                        <b-form-select
-                            :options="[1,2,3]"
-                            id="multiply-form_numberOfElements"
-                            size="sm"
-                        ></b-form-select>
-                    </b-form-group>
+                <div class="col-sm-12 col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <b-form-group :label="`Число примеров: ${this.numberOfItems}`">
+                                <b-form-input class="mt-2" v-model.number="numberOfItems" type="range" min="1" max="10"></b-form-input>
+                            </b-form-group>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <settings-item-crease
+                                        label="Скорость примеров"
+                                        :min="0"
+                                        :max="5000"
+                                        :step="500"
+                                        v-model="speedItems"
+                                    />
+                                </div>
+                                <div class="col-sm-6">
+                                    <settings-item-crease
+                                        label="Скорость слагаемых"
+                                        :min="0"
+                                        :max="5000"
+                                        :step="500"
+                                        v-model="speedElements"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <settings-item-crease
+                                        label="Число слагаемых"
+                                        :min="2"
+                                        :max="30"
+                                        v-model="numberOfElements"
+                                    />
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-9">
+                                    <div class="row mt-2">
+                                        <div class="col-sm-12">
+                                            <label>Размерность слагаемых</label>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <settings-item-crease
+                                                prepend="от"
+                                                :min="1"
+                                                :max="30"
+                                                v-model="sizeFrom"
+                                            />
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <settings-item-crease
+                                                prepend="до"
+                                                :min="1"
+                                                :max="30"
+                                                v-model="sizeTo"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-row">
+
+                    </div>
                 </div>
                 <div class="col-sm-6 col-lg-3">
 
@@ -26,12 +83,14 @@
 
 <script>
   import SettingsWrapper from '../common/SettingsWrapper'
+  import SettingsItemCrease from '../common/SettingsItemCrease';
 
   export default {
     name: "FlashAnzanSettings",
 
     components: {
       SettingsWrapper,
+      SettingsItemCrease,
     },
 
     props: {
@@ -43,8 +102,18 @@
 
     data: function () {
       return {
-        name: 'FlashAnzanSettings',
+        items: [],
+        speedItems: 1000,
+        speedElements: 500,
+        numberOfItems: 1,
+        numberOfElements: 2,
+        sizeFrom: 1,
+        sizeTo: 1,
       };
+    },
+
+    methods: {
+
     },
   }
 </script>
